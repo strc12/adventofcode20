40c3eb63743cef212360febc77dc35c3ed77877b
Rob Cunniffe <ric@oundleschool.org.uk>
2020-12-16 14:53:16 +0000

    day 15 done!

--- day15.py
+++ day15.py
@@ -1,24 +1,23 @@
 input1=[19,20,14,0,9,1]
 input0=[0,3,6]
 turn=1
-spoken=[0,3,6]
-spokpos={0:1,3:2,6:3}
-for k in range(3,20):
+spoken=[19,20,14,0,9,1]
+spokpos={19:0,20:1,14:2,0:3,9:4}
+for k in range(len(spoken)-1,30000005):
     lastnumber = spoken[len(spoken)-1]
-    spokpos[k-spokpos[lastnumber]]=k
-    #print(lastnumber)
-    prev=spoken[:len(spoken)-1]
-    if lastnumber not in spoken[:len(spoken)-1]:
-        print(lastnumber)
-        spoken.append(0)    
+    #print("lastnumber",lastnumber)
+    if lastnumber not in spokpos:
+        spokpos[lastnumber]=k
+        spoken.append(0)
+        #print(spoken,"0")
+        #print(spokpos,"0")
     else:
-        print(spokpos[lastnumber],k,k-spokpos[lastnumber],lastnumber)
-        spoken.append(k-spokpos[lastnumber])
+        newnum=k-spokpos[lastnumber]
+        #print("newnum",newnum)
+        spokpos[lastnumber]=k
+        spoken.append(newnum)
+        #print(spoken)
         
-        #spokpos[]
-    print(spokpos)
-    print(spoken)
-    #    nextspoken=0
-#print (spoken)
-print(spoken[1990:])
+        #print(spokpos)
+print(spoken[29999999])
 
